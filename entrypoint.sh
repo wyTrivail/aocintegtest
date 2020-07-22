c2a9539438d36584c1f037c1c8ef2d5888b2858c
Ying Wang <wangmyin@amazon.com>
2020-07-22 10:09:30 -0700

    remove the task_response from entrypoint since it's no need any more

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,3 @@ set -e
 # run task
 /app/aocintegtest/bin/aocintegtest $@
 
-# get response
-response=`cat task_response`
-
-# write response back to github action
-echo "::set-output name=task_response::${response}"

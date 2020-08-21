406f3acb32dcb8f5f54c4914d807cfa7b35c0d4b
Ying Wang <wangmyin@amazon.com>
2020-08-20 23:26:51 -0700

    add comments on the emitrer

--- src/main/java/com/amazon/aocagent/installers/emiterinstallers/OTEmitterInstaller.java
+++ src/main/java/com/amazon/aocagent/installers/emiterinstallers/OTEmitterInstaller.java
@@ -5,5 +5,9 @@ import com.amazon.aocagent.models.Context;
 public interface OTEmitterInstaller {
   void init(Context context) throws Exception;
 
+  /**
+   * Install the emitter and start it.
+   * @throws Exception when the emitter failed to install/start
+   */
   void installAndStart() throws Exception;
 }

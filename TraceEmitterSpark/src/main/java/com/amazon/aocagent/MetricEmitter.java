f9fcd2ab42231fc0d714fe91e1662b25182130e1
xiami <xiami@amazon.com>
2020-08-31 18:02:47 -0700

    add summary metric type

--- TraceEmitterSpark/src/main/java/com/amazon/aocagent/MetricEmitter.java
+++ TraceEmitterSpark/src/main/java/com/amazon/aocagent/MetricEmitter.java
@@ -3,32 +3,67 @@ package com.amazon.aocagent;
 import io.opentelemetry.OpenTelemetry;
 import io.opentelemetry.common.Labels;
 import io.opentelemetry.metrics.LongCounter;
+import io.opentelemetry.metrics.LongValueRecorder;
 import io.opentelemetry.metrics.Meter;
 
 public class MetricEmitter {
+
   static final String DIMENSION_API_NAME = "apiName";
   static final String DIMENSION_STATUS_CODE = "statusCode";
 
-  LongCounter returnTimeCounter;
+  static String API_COUNTER_METRIC = "apiBytesSent";
+  static String API_LATENCY_METRIC = "latency";
+
+  LongCounter apiBytesSentCounter;
+  LongValueRecorder apiLatencyRecorder;
+
 
   public MetricEmitter(String otlpEndpoint){
     Meter meter = OpenTelemetry.getMeter("cloudwatch-otel", "1.0");
 
     // give a instanceId appending to the metricname so that we can check the metric for each round of integ-test
-    String metricName = "latency";
+
+    String latencyMetricName = API_LATENCY_METRIC;
+    String apiBytesSentMetricName = API_COUNTER_METRIC;
     String instanceId = System.getenv("INSTANCE_ID");
     if(instanceId != null && !instanceId.trim().equals("")){
-      metricName += "_" + instanceId;
+      latencyMetricName += API_LATENCY_METRIC + "_" + instanceId;
+      apiBytesSentMetricName = API_COUNTER_METRIC + "_" + instanceId;
     }
-    returnTimeCounter = meter
-        .longCounterBuilder(metricName)
-        .setDescription("API return time")
-        .setUnit("ms")
+
+    apiBytesSentCounter = meter
+        .longCounterBuilder(apiBytesSentMetricName)
+        .setDescription("API request load sent in bytes")
+        .setUnit("one")
         .build();
+
+    apiLatencyRecorder =
+        meter
+            .longValueRecorderBuilder(latencyMetricName)
+            .setDescription("API latency time")
+            .setUnit("ms")
+            .build();
   }
 
+  /**
+   * emit http request latency metrics with summary metric type
+   * @param returnTime
+   * @param apiName
+   * @param statusCode
+   */
   public void emitReturnTimeMetric(Long returnTime, String apiName, String statusCode){
-    System.out.println("emit metric with returntime " + returnTime + "," + apiName + "," + statusCode);
-    returnTimeCounter.add(returnTime, Labels.of(DIMENSION_API_NAME, apiName, DIMENSION_STATUS_CODE, statusCode));
+    System.out.println("emit metric with return time " + returnTime + "," + apiName + "," + statusCode);
+    apiLatencyRecorder.record(returnTime, Labels.of(DIMENSION_API_NAME, apiName, DIMENSION_STATUS_CODE, statusCode));
+  }
+
+  /**
+   * emit http request load size with counter metrics type
+   * @param bytes
+   * @param apiName
+   * @param statusCode
+   */
+  public void emitBytesSentMetric(int bytes, String apiName, String statusCode) {
+    System.out.println("emit metric with http request size " + bytes + " byte, " + apiName);
+    apiBytesSentCounter.add(bytes, Labels.of(DIMENSION_API_NAME, apiName, DIMENSION_STATUS_CODE, statusCode));
   }
 }
